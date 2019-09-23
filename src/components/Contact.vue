e879d3316ef0f9db661e72dfc1861cf5ae7ffe01
Mark Robitaille <mark.rj.robitaille@gmail.com>
2019-09-22 19:37:34 -0500

    Created contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,20 +1,112 @@
 <template>
   <b-container fluid>
     <b-row align-h="center">
-      <b-col sm="11" class="page-content">
-        <h3>Contact Us</h3>
+      <b-col sm="11" class="pt-2 pb-2 page-content">
+        <b-jumbotron
+          class="mt-2"
+          header="Contact Us"
+          fluid
+        >Fill in the form below or contact us via social media.</b-jumbotron>
+        <b-row>
+          <b-col>
+            <b-form @submit="onSubmit" @reset="onReset" v-if="show" :disabled="formLoading">
+              <b-form-fieldset :disabled="formLoading">
+                <b-form-group label-cols="4" label-cols-lg="2" label="Given Name">
+                  <b-form-input v-model="form.name" required placeholder="Enter Given Name"></b-form-input>
+                </b-form-group>
+                <b-form-group label-cols="4" label-cols-lg="2" label="Surname">
+                  <b-form-input v-model="form.surname" required placeholder="Enter Surname"></b-form-input>
+                </b-form-group>
+                <b-form-group label-cols="4" label-cols-lg="2" label="Email:">
+                  <b-form-input v-model="form.email" required placeholder="Enter Email"></b-form-input>
+                </b-form-group>
+                <b-form-group label-cols="4" label-cols-lg="2" label="Message:">
+                  <b-form-input v-model="form.message" required placeholder="Enter Message"></b-form-input>
+                </b-form-group>
+                <b-row align-h="center">
+                  <b-col sm="2">
+                    <b-button type="submit" :variant="submitStatus" block>
+                      {{submitButton}}
+                      <b-spinner v-if="formLoading" class="ml-1" variant="light" small />
+                    </b-button>
+                  </b-col>
+                  <b-col sm="2">
+                    <b-button type="reset" variant="warning" block>Reset</b-button>
+                  </b-col>
+                </b-row>
+              </b-form-fieldset>
+            </b-form>
+          </b-col>
+        </b-row>
+        <b-row class="mt-4">
+          <b-col>
+            <Footer></Footer>
+          </b-col>
+        </b-row>
       </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
+import Footer from "@/components/Footer.vue";
+
 export default {
   name: "Events",
   data() {
-    return {};
+    return {
+      form: {
+        name: "",
+        surname: "",
+        email: "",
+        message: ""
+      },
+      show: true,
+      formLoading: false,
+      submitButton: "Submit",
+      submitStatus: "primary"
+    };
   },
-  components: {}
+  components: {
+    Footer
+  },
+  methods: {
+    onSubmit() {
+      let vm = this;
+      this.formLoading = true;
+      this.axios
+        .post("https://formcarry.com/s/XaTDp9NyI6I", this.form)
+        .then(res => {
+          console.log(res);
+          if (res.data && res.data.status == "success") {
+            vm.submitButton = "Submitted";
+            vm.submitStatus = "success";
+          } else {
+            vm.submitButton = "Error";
+            vm.submitStatus = "danger";
+          }
+          vm.formLoading = false;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    onReset() {
+      // Reset our form values
+      this.form.name = "";
+      this.form.surname = "";
+      this.form.email = "";
+      this.form.message = "";
+      this.formLoading = false;
+      this.submitButton = "Submit";
+      this.submitStatus = "primary";
+      // Reset/clear native browser form validation
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    }
+  }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Add BootstrapVue
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -10,21 +11,31 @@ import VueRouter from 'vue-router'
 // Get our routes
 import routes from './routes'
 
+// Add Axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+// Add FontAwesome icons and component
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faSlack, faFacebookSquare, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faEnvelope, faSlack, faFacebookSquare, faInstagram)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-
+// Use BootstrapVue
 Vue.use(BootstrapVue)
 
+// Use VueRouter
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes
 })
+ 
+// Use Axios
+Vue.use(VueAxios, axios)
+
+// Create library of emoticons and use FontAwesomeIcon
+library.add(faEnvelope, faSlack, faFacebookSquare, faInstagram)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
